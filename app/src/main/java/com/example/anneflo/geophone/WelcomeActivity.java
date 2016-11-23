3aa5b4f919d9f69ced3469f7f2af1bb093210198
julien95210 <Julien@MacBook-Air-de-Julien-2.local>
2016-11-23 11:07:44 +0100

    Adding vibror ring and home page

--- app/src/main/java/com/example/anneflo/geophone/WelcomeActivity.java
+++ app/src/main/java/com/example/anneflo/geophone/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.anneflo.geophone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
-}
+
+    protected void onStart(){
+        super.onStart();
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent startMain = new Intent(WelcomeActivity.this, LocationTest.class);
+                startActivity(startMain);
+            }
+        }, 5000L);
+    }}
+
